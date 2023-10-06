4579fd6cf0c9b14780ec99d2a3ad825c4cde6678
Gheorghita Stefana <gheorghitastefana@gmail.com>
2023-10-07 00:47:00 +0300

    'change'

---
+++ Tema1/Assets/Scripts/CactusIdleAndAttackTransition.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CactusIdleAndAttackTransition : StateMachineBehaviour
+{
+   // private Animator animator;
+   // private findOtherCactus otherCactus;
+    void Start()
+    {
+        //animator = GetComponent<Animator>();
+    }
+   
+   void Update()
+   {
+   // otherCactus = GameObject.FindObjectOfType<findOtherCactus>();
+   }
+}
