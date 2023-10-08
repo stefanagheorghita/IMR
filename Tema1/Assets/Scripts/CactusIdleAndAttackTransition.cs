9cfb13d864a38e41f3c1aef3a3a4eb317db39a28
Pascu Maria-Antonia-Emanuela <100427829+antoniamae@users.noreply.github.com>
2023-10-08 15:51:57 +0300

    Update CactusIdleAndAttackTransition.cs

--- Tema1/Assets/Scripts/CactusIdleAndAttackTransition.cs
+++ Tema1/Assets/Scripts/CactusIdleAndAttackTransition.cs
@@ -2,17 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CactusIdleAndAttackTransition : StateMachineBehaviour
+public class CactusIdleAndAttackTransition : MonoBehaviour
+{
+    [SerializeField] private GameObject firstCactus;
+    [SerializeField] private GameObject secondCactus;
+
+    private State firstCactusState;
+    private State secondCactusState;
+    private Animator firstCactusAnimator;
+    private Animator secondCactusAnimator;
+    public float distance;
+
+    void Start()
+    {
+        firstCactusState = firstCactus.GetComponent<State>();
+        secondCactusState = secondCactus.GetComponent<State>();
+        firstCactusAnimator = firstCactus.GetComponent<Animator>();
+        secondCactusAnimator = secondCactus.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (firstCactusState.exists && secondCactusState.exists)
+        {
+            attack();
+        }
+    }
+
+    public void attack()
+    {
+        distance = Vector3.Distance(firstCactus.transform.position, secondCactus.transform.position);
+        if (distance <= 25.0f)
+        {
+            animator.SetBool("Attack", true);
+        }
+        else
+        {
+            animator.SetBool("Attack", false);
+        }
+    }
+}
+
+
+
+
+
+
+
+/**public class CactusIdleAndAttackTransition : StateMachineBehaviour
 {
    // private Animator animator;
    // private findOtherCactus otherCactus;
-    void Start()
-    {
-        //animator = GetComponent<Animator>();
-    }
+   void Start()
+   {
+       // animator = GetComponent<Animator>();
+   }
    
    void Update()
    {
-   // otherCactus = GameObject.FindObjectOfType<findOtherCactus>();
+       // otherCactus = GameObject.FindObjectOfType<findOtherCactus>();
    }
-}
+}**/
