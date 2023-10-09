ed64b8b854eb77c525985d5fb8b5442838fe3d87
Gheorghita Stefana <gheorghitastefana@gmail.com>
2023-10-09 21:21:34 +0300

    changes

--- Tema1/Assets/Scripts/CactusIdleAndAttackTransition.cs
+++ Tema1/Assets/Scripts/CactusIdleAndAttackTransition.cs
@@ -7,60 +7,38 @@ public class CactusIdleAndAttackTransition : MonoBehaviour
     [SerializeField] private GameObject firstCactus;
     [SerializeField] private GameObject secondCactus;
 
-    private State firstCactusState;
-    private State secondCactusState;
     private Animator firstCactusAnimator;
     private Animator secondCactusAnimator;
     public float distance;
 
     void Start()
     {
-        firstCactusState = firstCactus.GetComponent<State>();
-        secondCactusState = secondCactus.GetComponent<State>();
         firstCactusAnimator = firstCactus.GetComponent<Animator>();
         secondCactusAnimator = secondCactus.GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        if (firstCactusState.exists && secondCactusState.exists)
-        {
-            attack();
-        }
-    }
-
-    public void attack()
-    {
         distance = Vector3.Distance(firstCactus.transform.position, secondCactus.transform.position);
-        if (distance <= 25.0f)
+        if (distance <= 0.25f)
         {
-            firstCactusAnimator.SetBool("Attack", true);
+            firstCactusAnimator.SetBool("isAttacking", true);
+
         }
         else
         {
-            firstCactusAnimator.SetBool("Attack", false);
+            firstCactusAnimator.SetBool("isAttacking", false);
+
         }
     }
+       
+    
+
+   
+    
+        
 }
 
 
 
-
-
-
-
-/**public class CactusIdleAndAttackTransition : StateMachineBehaviour
-{
-   // private Animator animator;
-   // private findOtherCactus otherCactus;
-   void Start()
-   {
-       // animator = GetComponent<Animator>();
-   }
-   
-   void Update()
-   {
-       // otherCactus = GameObject.FindObjectOfType<findOtherCactus>();
-   }
-}**/
